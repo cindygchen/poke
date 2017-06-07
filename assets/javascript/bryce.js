6ebdce38e670cf5a1e5af82658ee7edbdaaecddd
bcmiller713 <bcmiller713@gmail.com>
2017-06-06 20:07:07 -0700

    blah

---
+++ assets/javascript/bryce.js
@@ -0,0 +1,29 @@
+// Splash page navigation/input
+$("#eat-button").on("click", function() {
+	//showEatOptions
+});
+$("#day-button").on("click", function() {
+	//showDayOptions	
+});
+$("#night-button").on("click", function() {
+	//showNightOptions	
+});
+$("#other-button")on("click", function() {
+	//showOtherOptions	
+});
+
+
+// When user completes splash page navigation/input, display results and map
+$(".placeholder-button").on("click", function() {
+	//showResults	
+});
+$(".placeholder-button").on("click", function() {
+	//showMapTags	
+});
+
+
+// When you click on a result, it expands to show more details
+$(".result-item").on("click", function() {
+	//expandDetails	
+});
+
