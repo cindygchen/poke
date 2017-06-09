d41c98d0a62defcb5d305aae41174533227641c5
Omnomon <jessselu@gmail.com>
2017-06-08 21:40:56 -0700

    change places.js to include descriptions

--- assets/javascript/places.js
+++ assets/javascript/places.js
@@ -3,40 +3,40 @@ var searchObjectNameLocationAndPhotoID = []
 
 
 $.ajax({
-  url: "https://maps.googleapis.com/maps/api/place/textsearch/json?query="+ query +"&location=32.7157380,-117.1610840&radius=50000&key=AIzaSyBSmftseE9huym0ariNTCamMnQmMZYaDYw"
+    url: "https://maps.googleapis.com/maps/api/place/textsearch/json?query=" + query + "&location=32.7157380,-117.1610840&radius=50000&key=AIzaSyBSmftseE9huym0ariNTCamMnQmMZYaDYw"
 
-}).done(function(response){
-  var nameArray =  [];
-  var photoIDArray = [];
-  var coordArray = [];
-  var coord = [];
-  var lat;
-  var lng;
-  var photoID = [];
-  var name;
-  var items = response.results
-  console.log(items)
-  console.log(items[0].photos[0].photo_reference)
-  for (let value of items) {
-    lat = value.geometry.location.lat 
-    lng = value.geometry.location.lng
-    console.log(value)
-    photoID = value.photos && value.photos[0].photo_reference
-    name = value.name 
-    coord = [lat,lng]
-    nameArray.push(name)
-    photoIDArray.push(photoID)
-    coordArray.push(coord)
-  }
-    for (i = 0; i < nameArray.length; i++) {
-    var attachToPin = {
-      name: nameArray[i],
-      lat: coordArray[i][0],
-      lng: coordArray[i][1],
-      photoID: photoIDArray[i]
+}).done(function(response) {
+
+    var coord = [];
+    var photoID = [];
+    var placeID;
+    var name;
+    var items = response.results
+    var description;
+    console.log(items)
+    console.log(items[0].photos[0].photo_reference)
+    for (let value of items) {
+        var lat = value.geometry.location.lat
+        var lng = value.geometry.location.lng
+        console.log(value)
+        photoID = value.photos && value.photos[0].photo_reference
+        name = value.name
+        placeID = value.place_id
+        $.ajax({
+            url: "https://maps.googleapis.com/maps/api/place/details/json?placeid=" + placeID + "&key=AIzaSyBSmftseE9huym0ariNTCamMnQmMZYaDYw"
+        }).done(function(response) {
+            console.log(response)
+            description = response.result.reviews
+            console.log(description)
+            var attachToPin = {
+                name: name,
+                lat: lat,
+                lng: lng,
+                photoID: photoID,
+                description: description
+            }
+            searchObjectNameLocationAndPhotoID.push(attachToPin)
+        })
     }
-    searchObjectNameLocationAndPhotoID.push(attachToPin)
-  }
-
-  return searchObjectNameLocationAndPhotoID
+    return searchObjectNameLocationAndPhotoID
 })
